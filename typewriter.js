98342ff36502f4b8328116059354dada8290d6f3
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-06 15:36:21 +0000

    Creating a typewriter app which prints out a given string by delaying each character. This was achieved by looping through each string character, and using the setTimeOut() method within the loop, incrementing the count of the delay on every iteration.

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const string = "Hello my name is Matt and I can't wait to start learning about async!";
+let counter = 50;
+
+for (const char of string) {
+  i = 0;
+  counter += 50;
+  
+  setTimeout(() => {
+    process.stdout.write(char);
+    i++
+    if (i === string.length) {
+      console.log("\n")
+    }
+  }, 1000 + counter);
+};
+
